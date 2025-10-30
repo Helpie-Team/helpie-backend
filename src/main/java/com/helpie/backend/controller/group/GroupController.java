2da3c43e9de221cca0c4aa5d468ae5d3498c6ffc
dnwldla <leeemjw@naver.com>
2025-10-30 16:32:29 +0900

    feat: 카테고리별 소모임을 조회하는 API 구현

--- src/main/java/com/helpie/backend/controller/group/GroupController.java
+++ src/main/java/com/helpie/backend/controller/group/GroupController.java
@@ -1,14 +1,22 @@
 package com.helpie.backend.controller.group;
 
+import com.helpie.backend.domain.group.Category;
 import com.helpie.backend.dto.group.GroupCreateRequest;
 import com.helpie.backend.dto.group.GroupCreateResponse;
+import com.helpie.backend.dto.group.GroupResponse;
 import com.helpie.backend.service.group.GroupService;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +26,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "소모임", description = "소모임 관련 API")
 @RequestMapping("/api/v1/group")
 public class GroupController {
 
@@ -32,6 +41,17 @@ public class GroupController {
 
         GroupCreateResponse response = groupService.createGroup(userId,request, images);
 
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<Page<GroupResponse>> getGroups(
+        @Parameter(description = "소모임 카테고리") @RequestParam Category category,
+        @Parameter(description = "사용자 ID") @RequestParam Long userId,
+        @RequestParam(defaultValue ="0") int page,
+        @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+    ){
+        Page<GroupResponse> response=groupService.getGroups(userId,category,pageable);
         return ResponseEntity.ok(response);
     }
 
