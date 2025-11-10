14db6d6135ed2f7a342cb1ae16cbfc4947280141
dnwldla <94666277+dnwldla@users.noreply.github.com>
2025-11-10 10:01:53 +0900

    페이지네이션 기본값 수정 (#82)
    * fix: 소모임 조회 시 이미지 조회 가능하게 수정
    
    * feat: 디폴트 썸네일 추가
    
    * fix: 기본 페이지 크기를 20개에서 12개로 변경

--- src/main/java/com/helpie/backend/controller/group/GroupController.java
+++ src/main/java/com/helpie/backend/controller/group/GroupController.java
@@ -93,7 +93,7 @@ public class GroupController {
         @Parameter(description = "나라") @RequestParam String country,
         @Parameter(description = "소모임 카테고리") @RequestParam Category category,
         @RequestParam(defaultValue ="0") int page,
-        @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+        @PageableDefault(size = 12, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
     ){
         Page<GroupResponse> response=groupService.getGroupByCountry(userVo.getId(),country,category,pageable);
         return ResponseEntity.ok(response);
