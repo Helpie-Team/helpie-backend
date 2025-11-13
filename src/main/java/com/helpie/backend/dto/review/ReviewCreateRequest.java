5cbb8b02e36777c391559cf13ceb13bb4eb75b71
dnwldla <leeemjw@naver.com>
2025-11-13 09:45:31 +0900

    feat: 전체 국가 조회 api 추가

--- src/main/java/com/helpie/backend/dto/review/ReviewCreateRequest.java
+++ src/main/java/com/helpie/backend/dto/review/ReviewCreateRequest.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "리뷰 생성 요청",
     example = """
         {
-          "rate": "5,
+          "rate": "5",
           "description": "진짜 재밌는 소모임이었다..",
           "anonymityYn": false
         }
