cdeca47157ff70a4582db037bacab6ba74bd7533
dnwldla <leeemjw@naver.com>
2025-11-15 19:38:29 +0900

    feat: 중복 리뷰 예외처리 추가

--- src/main/java/com/helpie/backend/repository/review/ReviewRepository.java
+++ src/main/java/com/helpie/backend/repository/review/ReviewRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
+    boolean existsByGroup_IdAndUser_Id(Long groupId, Long userId);
 }
