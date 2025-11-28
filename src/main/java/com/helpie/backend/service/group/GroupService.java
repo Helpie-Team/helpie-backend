b236ac88b262f2364e743ab53222a069426ef10c
dnwldla <94666277+dnwldla@users.noreply.github.com>
2025-11-29 02:26:25 +0900

    Modify group retrieval to include userId

--- src/main/java/com/helpie/backend/service/group/GroupService.java
+++ src/main/java/com/helpie/backend/service/group/GroupService.java
@@ -218,7 +218,7 @@ public class GroupService {
             return RecommendedResponse.locked("SURVEY_REQUIRED", pageable);
         }
 
-        Page<Group> groups=groupRepository.findByInterestFiltersV2(pageable);
+        Page<Group> groups=groupRepository.findByInterestFiltersV2(userId,pageable);
 
 
         Page<GroupResponse> responsePage = mapGroupsWithBookmarks(userId, groups);
